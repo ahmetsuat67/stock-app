0e252592d1e8586a188f04c41eaeb8593cee271c
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2023-09-05 22:35:25 +0300

    adding product

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -6,14 +6,17 @@ describe("products-test", () => {
 
     it("products", () => {
         cy.login()
-
         cy.get("[data-test='titleProducts']").contains("Products").should("be.visible").click({ force: true });
         cy.get("[data-test='productsHeader']").contains("Products").should("be.visible");
-        cy.get("[data-test='productsBtn']").contains("NEW PRODUCT").should("be.visible").click({ force: true });
-        cy.get("[data-test='productsCat']").contains("Categories")
-        cy.get("[data-testid='ArrowDropDownIcon']").click({ multiple: true })
+        cy.wait(20000)
+        cy.get("[data-test='productsBtn']",{ timeout: 20000 }).contains("NEW PRODUCT").should("be.visible").click({ force: true });
+        cy.get("[data-test='productsCat']",{ timeout: 20000 }).contains("Categories")
+        
+        cy.get("#category").click({ force: true })
+        cy.get("[data-test='Drink']").click({ force: true })
+        
 
-        cy.get("[data-test='productsBrands']").contains("Brands").click({ force: true }).type("https://docs.cypress.io/img/logo/cypress-logo-dark.png");
+        cy.get("[data-test='productsBrands']").contains("Brands").click({ force: true })
         cy.get("[data-test='productsName']").contains("Product Name").should("be.visible").click({ force: true });
         cy.get("[data-test='productsSbmt']").contains("Submit").should("be.visible").click({ force: true });
         cy.get("[data-test='productsListName']").should("be.visible");
